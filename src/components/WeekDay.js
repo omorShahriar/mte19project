813702b3c3d254ac1d29c015a88abcdfadf7065f
omorShahriar <omor.shahriar2000@gmail.com>
2020-10-20 09:19:35 +0600

    remove auto collapse

--- src/components/WeekDay.js
+++ src/components/WeekDay.js
@@ -9,29 +9,15 @@ import style from './weekday.module.css'
 import Course from '../components/Course'
 
 const WeekDay = ({info}) => {
-       const node = useRef()
+       
     const [open, setOpen] = useState(false)
-    useEffect(() => {
-  // add when mounted
-  document.addEventListener("mousedown", handleClick);
-  // return function to be called when unmounted
-  return () => {
-    document.removeEventListener("mousedown", handleClick);
-  };
-}, []);
-    const handleClick = (e) => {
-        if (node.current.contains(e.target)) {
-             // inside click
-            return;
-         }
-            setOpen(false)
-    }
+
     return (
         
             
             <div className={`${style.test} ${open && style.collapseOpen}` } sx={{textAlign:'center',backgroundColor:'#333',height:50,color:'#f2f2f2'}}>
             
-                <h3 ref={node} className={`${style.header} ${open && style.headerShadow}`} onClick={() => setOpen(!open)}>{info.day}</h3>
+                <h3  className={`${style.header} ${open && style.headerShadow}`} onClick={() => setOpen(!open)}>{info.day}</h3>
                 <CSSTransition in={open} timeout={{
                     enter: 100,
                     exit: 100,
